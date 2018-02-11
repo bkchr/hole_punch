835f2960c6c6a3eba0f113a5b484fae41151c494
Bastian Köcher <git@kchr.de>
2018-02-11 16:50:01 +0100

    More context stuff

--- src/context.rs
+++ src/context.rs
@@ -1,8 +1,35 @@
 use errors::*;
 use strategies;
+use config::Config;
 
-use futures::stream::FuturesUnordered;
+use std::marker::PhantomData;
 
-pub struct Context {
+use futures::stream::{futures_unordered, FuturesUnordered, StreamFuture};
+use futures::Stream;
+
+use tokio_core::reactor::Handle;
+
+pub struct Context<P> {
+    strategies: FuturesUnordered<StreamFuture<strategies::Strategy>>,
+    _marker: PhantomData<P>,
+}
+
+impl<P> Context<P> {
+    fn new(handle: Handle, config: Config) -> Result<Context<P>> {
+        let strats =
+            strategies::init(handle, &config).chain_err(|| "error initializing the strategies")?;
+
+        Ok(Context {
+            strategies: futures_unordered(strats.into_iter().map(|s| s.into_future())),
+        })
+    }
+}
+
+impl<P> Stream for Context<P> {
+    type Item = Connection<P>;
+    type Error = Error;
+}
+
+struct Connection<P> {
     
 }
