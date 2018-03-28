008b6c22f0bd5672fe4b7da7e2feaabb3f9579c3
Bastian Köcher <git@kchr.de>
2018-03-28 12:34:29 +0200

    Adapt the test scripts to compile the test runners

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-tmpdir=$(dirname $(mktemp -u))
+tmp_dir=$(dirname $(mktemp -u))
+docker_home=$tmp_dir/hole_punch_test_docker_home
+mkdir -p $docker_home
+
 enable_kvm=""
 
 if [ -e /dev/kvm ]; then
   enable_kvm="--device /dev/kvm"
 fi
 
-docker run -it $enable_kvm --rm -v $tmpdir/hole_punch_test_docker_home:/root -v $PWD/..:/src -w /src/tests bkchr/hole_punch_tester "./run_in_docker.sh"
+docker run -it $enable_kvm --rm -v $docker_home:/root -v $PWD/..:/src -w /src/tests bkchr/hole_punch_tester "./run_in_docker.sh"
