83dc30a75be3cd761bd34ee5b25a672f6c72ccf0
Bastian Köcher <git@kchr.de>
2019-09-17 11:13:03 +0200

    Make sure we don't die because of failing to register the mDNS service

--- src/registries/mdns.rs
+++ src/registries/mdns.rs
@@ -8,7 +8,7 @@ use crate::{
     PubKeyHash,
 };
 
-use libmdns::{Responder, Service};
+use libmdns::{Responder, ResponderTask, Service};
 
 use mdns::RecordKind;
 
@@ -31,15 +31,16 @@ use futures::{
 
 use lru_time_cache::LruCache;
 
+use state_machine_future::{transition, RentToOwn, StateMachineFuture};
+
 type PeerRequest = (oneshot::Sender<RegistryResult>, PubKeyHash);
 type RequestReceiver = UnboundedReceiver<PeerRequest>;
 
 /// A registry that registers the local peer in local DNS (mDNS) and also searches for other peers
 /// in local DNS.
 pub struct MdnsRegistry {
-    _responder: Responder,
-    _services: Vec<Service>,
     request_sender: UnboundedSender<(oneshot::Sender<RegistryResult>, PubKeyHash)>,
+    _responder_handle: oneshot::Sender<()>,
 }
 
 impl MdnsRegistry {
@@ -57,19 +58,14 @@ impl MdnsRegistry {
             format!("_{}", service_name)
         };
 
-        let local_peer_identifier_string = local_peer_identifier.to_string();
-        let responder = Responder::new()?;
-        let service = responder.register(
+        let (context, _responder_handle) = MdnsResponderContext::create(
             service_name.clone(),
-            // The hash is to long and we need to split it up.
-            format!(
-                "{}.{}",
-                &local_peer_identifier_string[..local_peer_identifier_string.len() / 2],
-                &local_peer_identifier_string[local_peer_identifier_string.len() / 2..],
-            ),
+            Duration::from_secs(5),
+            local_peer_identifier.clone(),
             listen_port,
-            &[],
         );
+        let responder = MdnsResponder::start(context);
+        handle.spawn(responder.map_err(|_| ()));
 
         let (request_sender, request_recv) = mpsc::unbounded();
 
@@ -82,9 +78,8 @@ impl MdnsRegistry {
         )?);
 
         Ok(MdnsRegistry {
-            _responder: responder,
-            _services: vec![service],
             request_sender,
+            _responder_handle,
         })
     }
 }
@@ -262,6 +257,157 @@ impl Future for Discovery {
                     })
                     .map_err(|_| ()),
             );
+        }
+    }
+}
+
+enum Never {}
+
+struct MdnsResponderContext {
+    /// Time to wait before retrying the registration of the responder.
+    wait_before_retry: Duration,
+    /// The identifier of this peer.
+    local_peer_identifier: String,
+    service_name: String,
+    /// Tells us when we should shutdown.
+    shutdown_signal: oneshot::Receiver<()>,
+    listen_port: u16,
+}
+
+impl MdnsResponderContext {
+    fn create(
+        service_name: String,
+        wait_before_retry: Duration,
+        local_peer_identifier: PubKeyHash,
+        listen_port: u16,
+    ) -> (Self, oneshot::Sender<()>) {
+        // The hash is to long and we need to split it up.
+        let local_peer_identifier_string = local_peer_identifier.to_string();
+        let local_peer_identifier = format!(
+            "{}.{}",
+            &local_peer_identifier_string[..local_peer_identifier_string.len() / 2],
+            &local_peer_identifier_string[local_peer_identifier_string.len() / 2..],
+        );
+
+        let (sender, shutdown_signal) = oneshot::channel();
+        (
+            Self {
+                service_name,
+                shutdown_signal,
+                wait_before_retry,
+                local_peer_identifier,
+                listen_port,
+            },
+            sender,
+        )
+    }
+
+    fn is_shutdown(&mut self) -> bool {
+        self.shutdown_signal
+            .poll()
+            .map(|v| v.is_ready())
+            .unwrap_or(true)
+    }
+}
+
+/// MDNs responder state machine. This makes sure we have the responder registered/reregistered.
+///
+/// The responder can only be registered when we have a valid network device with some connection to
+/// something. So, if any error occurs while registering/polling we just retry after some timeout.
+#[derive(StateMachineFuture)]
+#[state_machine_future(context = "MdnsResponderContext")]
+enum MdnsResponder {
+    /// Registers the responder.
+    ///
+    /// On success it jumps to `PollResponder`.
+    ///
+    /// On error it jumps to `WaitForRetry`.
+    ///
+    /// Jumps to `ReadyState` when we should shutdown.
+    #[state_machine_future(start, transitions(PollResponder, WaitForRetry, ReadyState))]
+    RegisterResponder,
+    /// Polls the responder.
+    ///
+    /// On return value that is not `NotReady` it jumps to `WaitForRetry`.
+    ///
+    /// Jumps to `ReadyState` when we should shutdown.
+    #[state_machine_future(transitions(WaitForRetry, ReadyState))]
+    PollResponder {
+        _responder: Responder,
+        responder_task: ResponderTask,
+        _service: Service,
+    },
+    #[state_machine_future(transitions(RegisterResponder, ReadyState))]
+    WaitForRetry(Delay),
+    #[state_machine_future(ready)]
+    ReadyState(()),
+    #[state_machine_future(error)]
+    ErrorState(Never),
+}
+
+impl PollMdnsResponder for MdnsResponder {
+    fn poll_register_responder<'a, 'c>(
+        _: &'a mut RentToOwn<'a, RegisterResponder>,
+        context: &'c mut RentToOwn<'c, MdnsResponderContext>,
+    ) -> Poll<AfterRegisterResponder, Never> {
+        if context.is_shutdown() {
+            transition!(ReadyState(()))
+        }
+
+        if let Ok((_responder, responder_task)) = Responder::create() {
+            let _service = _responder.register(
+                context.service_name.clone(),
+                context.local_peer_identifier.clone(),
+                context.listen_port,
+                &[],
+            );
+
+            transition!(PollResponder {
+                _responder,
+                responder_task,
+                _service
+            })
+        } else {
+            transition!(WaitForRetry(Delay::new(
+                Instant::now() + context.wait_before_retry
+            )))
+        }
+    }
+
+    fn poll_poll_responder<'a, 'c>(
+        state: &'a mut RentToOwn<'a, PollResponder>,
+        context: &'c mut RentToOwn<'c, MdnsResponderContext>,
+    ) -> Poll<AfterPollResponder, Never> {
+        if context.is_shutdown() {
+            transition!(ReadyState(()))
+        }
+
+        if state
+            .responder_task
+            .poll()
+            .map(|v| v.is_ready())
+            .unwrap_or(true)
+        {
+            transition!(WaitForRetry(Delay::new(
+                Instant::now() + context.wait_before_retry
+            )))
+        } else {
+            Ok(NotReady)
+        }
+    }
+
+    fn poll_wait_for_retry<'a, 'c>(
+        state: &'a mut RentToOwn<'a, WaitForRetry>,
+        context: &'c mut RentToOwn<'c, MdnsResponderContext>,
+    ) -> Poll<AfterWaitForRetry, Never> {
+        if context.is_shutdown() {
+            transition!(ReadyState(()))
+        }
+
+        if state.0.poll().map(|v| v.is_ready()).unwrap_or(true) {
+            transition!(RegisterResponder)
+        } else {
+            Ok(NotReady)
         }
     }
 }
